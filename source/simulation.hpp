229e849c149392f4a274b10fb3cc810ff25ac4bd
Lucas Lopes <lucas1noid@gmail.com>
2025-12-04 08:53:44 -0300

    add: enum to state machine

--- source/simulation.hpp
+++ source/simulation.hpp
@@ -3,7 +3,14 @@
 namespace sg {
     class SnazeSimulation {
     private:
-    
+      enum game_state_e : std::uint8_t {
+        WELCOME = 0, //! Boas vindas
+        STARTING,    //! Tela inicial do jogo
+        PLAYING,     //! Jogando (player ou IA)
+        VICTORY,     //! Vitória
+        GAMEOVER     //! Derrota
+        ENDING,      //! Finaliza o jogo
+      };
     public:
      static bool is_over();
      static void process_events();
